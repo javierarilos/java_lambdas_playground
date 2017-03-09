3c65940a2ebe24bbce842e7077c4f57dcef0b6c2
javierarilos <javier.arilos@gmail.com>
2017-03-09 08:37:38 +0100

    pom.xml lang level 1.8

---
+++ src/main/java/io/Iterators.java
@@ -0,0 +1,36 @@
+package io;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+
+        System.out.println( "----- EXTERNAL ITERATORS: FOR" );
+
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+
+        System.out.println( "----- EXTERNAL ITERATORS: FOREACH" );
+
+        for (int current :numbers) {
+            System.out.println(current);
+        }
+
+        System.out.println( "----- INTERNAL ITERATOR" );
+
+        numbers.forEach(n -> System.out.println(n));
+
+        System.out.println( "----- INTERNAL ITERATOR: WITH METHOD REFERENCE" );
+
+        numbers.forEach(System.out::println);
+    }
+}
